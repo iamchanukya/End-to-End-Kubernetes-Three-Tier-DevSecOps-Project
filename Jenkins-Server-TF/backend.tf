a01f11875a5bcaf083cb258f0a2134390461ac6b
iamchanukya <160751471+iamchanukya@users.noreply.github.com>
2024-08-23 22:56:12 +0530

    Update backend.tf

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "mernstack-demo-bucket"
+    bucket         = "my-ews-baket1"
     region         = "us-east-1"
-    key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "mernstack"
+    key            = "eks/terraform.tfstate"
+    dynamodb_table = "Lock-Files"
     encrypt        = true
   }
   required_version = ">=0.13.0"
